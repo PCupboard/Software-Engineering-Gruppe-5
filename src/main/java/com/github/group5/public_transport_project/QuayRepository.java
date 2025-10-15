19759dc08313231625c9b43b2e3cbce440541fc2
WilhelmNorve <143356195+WilhelmNorve@users.noreply.github.com>
2025-10-15 11:04:21 +0200

    Update QuayRepository.java
    la til kode i klassen

--- src/main/java/com/github/group5/public_transport_project/QuayRepository.java
+++ src/main/java/com/github/group5/public_transport_project/QuayRepository.java
@@ -1,5 +1,26 @@
-﻿package com.github.group5.public_transport_project;
+package com.github.group5.public_transport_project;
 
-public class QuayRepository {
-    // TODO: implementer QuayRepository
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+
+public final class QuayRepository {
+      private static List<QuayLoader.Quay> quays = List.of(); 
+
+    private QuayRepository() {}
+
+    public static void init(Path jsonPath) throws IOException {
+        ArrayList<QuayLoader.Quay> loaded = QuayLoader.load(jsonPath);
+        
+        quays = Collections.unmodifiableList(loaded);
+    }
+
+    public static List<QuayLoader.Quay> getQuays() {
+        return quays;
+    }
+    
 }
+
